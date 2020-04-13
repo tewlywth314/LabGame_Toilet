40798180689a6529eff9bb32610852ef7b106588
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2020-04-13 15:57:30 +0700

    4/13/2020     Movement Fixed

---
+++ Assets/Scripts/betterJump.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class betterJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float FallMultiplier = 2.5f;
+    [SerializeField] float LowJumpMultiplier = 2f;
+    [SerializeField] KeyCode MoveUp;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up *Physics.gravity.y * (FallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y < 0 && Input.GetKeyDown(MoveUp))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (LowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
