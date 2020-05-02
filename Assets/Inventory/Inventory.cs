fad4b02582972d599fe8e1f8f5eee7800228b80b
Miyasaki-Kei <61342142+Miyasaki-Kei@users.noreply.github.com>
2020-05-02 19:38:00 +0700

    Inventory.beta

---
+++ Assets/Inventory/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject itemtext;
+    bool set = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            
+            if (set == false)
+            {
+                set = true;
+                canvas.SetActive(set);
+            }
+            else
+            {
+                set = false;
+                canvas.SetActive(set);
+            }
+        }
+    }
+}
