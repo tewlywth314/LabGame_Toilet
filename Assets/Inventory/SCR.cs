456688addbd9da38bfb4095beb01d204473a83d6
Miyasaki-Kei <61342142+Miyasaki-Kei@users.noreply.github.com>
2020-05-06 01:59:15 +0700

    เปลี่ยนซีนตำแหน่งไม่ย้อน

--- Assets/Inventory/SCR.cs
+++ Assets/Inventory/SCR.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SCR : MonoBehaviour
 {
+    float X, Y, Z;
+    void Start()
+    {
+        X = PlayerPrefs.GetFloat("x");
+        Y = PlayerPrefs.GetFloat("y");
+        Z = PlayerPrefs.GetFloat("z");
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            PlayerPrefs.SetInt("Ch", 1);
+            PlayerPrefs.SetFloat("x", X);
+            PlayerPrefs.SetFloat("y", Y);
+            PlayerPrefs.SetFloat("z", Z);
             SceneManager.LoadSceneAsync(PlayerPrefs.GetString("CS"));
+            
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,25 +10,48 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Animator anim;
     Scene scene;
     float rotate = 180;
+    float X, Y, Z;
     // Start is called before the first frame update
     void Start()
     {
         
+        if (PlayerPrefs.GetInt("Ch") == 1)
+        {
+            X = PlayerPrefs.GetFloat("x");
+            Y = PlayerPrefs.GetFloat("y");
+            Z = PlayerPrefs.GetFloat("z");
+            rb.transform.position = new Vector3(X, Y, Z);
+            PlayerPrefs.SetInt("Ch", 0);
+        }
+        else
+        {
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.F5))
+        {
+            PlayerPrefs.DeleteAll();
+        }
         if(Input.GetKeyDown(KeyCode.I))
         {
             scene = SceneManager.GetActiveScene();
             PlayerPrefs.SetString("CS", scene.name);
-            Debug.Log(scene.name);
             SceneManager.LoadSceneAsync("Inventory");
+            X = rb.transform.position.x;
+            Y = rb.transform.position.y;
+            Z = rb.transform.position.z;
+            PlayerPrefs.SetFloat("x", X);
+            PlayerPrefs.SetFloat("y", Y);
+            PlayerPrefs.SetFloat("z", Z);
         }
         if(Input.GetKey(KeyCode.D))
         {
-            rb.velocity = new Vector3( speed * Time.deltaTime,0f,0f);
+            rb.transform.position = rb.transform.position + new Vector3( speed * Time.deltaTime,0f,0f);
             anim.SetBool("IsWalking", true);
             if (rotate != 0)
             {
@@ -39,7 +62,7 @@ public class PlayerController : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = new Vector3(-1*speed * Time.deltaTime, 0f, 0f);
+            rb.transform.position = rb.transform.position - new Vector3(speed * Time.deltaTime, 0f, 0f);
             anim.SetBool("IsWalking", true);
             if (rotate != 180)
             {
