251a7572220be89899c37aaebecf5baeb30ff1b0
Miyasaki-Kei <61342142+Miyasaki-Kei@users.noreply.github.com>
2020-05-05 17:03:36 +0700

    ซ่อนไอเท็ม

--- Assets/Inventory/Inventory.cs
+++ Assets/Inventory/Inventory.cs
@@ -6,6 +6,22 @@ public class Inventory : MonoBehaviour
 {
     [SerializeField] GameObject canvas;
     [SerializeField] GameObject itemtext;
+    [SerializeField] GameObject Toothbrush;
+    [SerializeField] GameObject ToothbrushOnGround;
+    [SerializeField] GameObject Toothpaste;
+    [SerializeField] GameObject Bowl;
+    [SerializeField] GameObject Shaving;
+    [SerializeField] GameObject ScrubberBrush;
+    [SerializeField] GameObject ToiletSuction;
+    [SerializeField] GameObject Comb;
+    [SerializeField] GameObject Sprinkler;
+    [SerializeField] GameObject DishwashingLiquid;
+    [SerializeField] GameObject Sponge;
+    [SerializeField] GameObject ShoePolish;
+    [SerializeField] GameObject Fabric;
+    [SerializeField] GameObject Soap;
+    [SerializeField] GameObject Shampoo;
+    [SerializeField] GameObject Closet;
     bool set = false;
     void Update()
     {
@@ -22,6 +38,14 @@ public class Inventory : MonoBehaviour
                 set = false;
                 canvas.SetActive(set);
             }
+        }
+        if(ToothbrushOnGround.activeSelf == false)
+        {
+            Toothbrush.SetActive(true);
+        }
+        else
+        {
+            
         }
     }
 }
