6fd76f5bbc313320f5f58295a8dfe0935a9c2f9a
Miyasaki-Kei <61342142+Miyasaki-Kei@users.noreply.github.com>
2020-05-16 12:12:08 +0700

    Stop

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] GameObject Content;
+    [SerializeField] Button Button;
+    private void Update()
+    {
+        Button.transform.SetParent(Content.transform,true);
+    }
+}
