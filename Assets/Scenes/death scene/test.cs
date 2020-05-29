1e680f82caa6f5192413ad24c93692f8257fa687
ittinan99 <61344879+ittinan99@users.noreply.github.com>
2020-05-29 17:27:38 +0700

    add Deathscene

---
+++ Assets/Scenes/death scene/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour
+{
+    public static string ThisScene;
+    void Start()
+    {
+        ThisScene = SceneManager.GetActiveScene().name;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadSceneAsync("deathscene");
+        }
+    }
+}
