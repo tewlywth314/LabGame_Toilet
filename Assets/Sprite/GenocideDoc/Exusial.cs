d148e2262a587e10cd590d5de9108660f59a7019
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2020-06-17 01:29:31 +0700

    Sound Chen

--- Assets/Sprite/GenocideDoc/Exusial.cs
+++ Assets/Sprite/GenocideDoc/Exusial.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Exusial : MonoBehaviour
 {
     private Animator anim;
+    private AudioSource Audio;
+    public AudioClip Deployed;
+    public AudioClip ApplePie;
+    public AudioClip skill3;
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        Audio = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
         anim.SetBool("End", false);
+        Audio.PlayOneShot(Deployed);
         StartCoroutine(Barrage());
+      
     }
 
     // Update is called once per frame
@@ -20,7 +30,12 @@ public class Exusial : MonoBehaviour
     }
     IEnumerator Barrage()
     {
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(1f);
+        Audio.PlayOneShot(ApplePie);
+        yield return new WaitForSeconds(1.2f);
+        Audio.PlayOneShot(skill3,1f);
+        yield return new WaitForSeconds(6f);
+        
         anim.SetBool("End", true);
     }
 }
--- Assets/Sprite/GenocideDoc/SpawnSlash.cs
+++ Assets/Sprite/GenocideDoc/SpawnSlash.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SpawnSlash : MonoBehaviour
 {
@@ -8,12 +9,17 @@ public class SpawnSlash : MonoBehaviour
     public GameObject Slash;
     private Vector3 error;
     private Vector3 Pos;
+    public AudioClip ChenDeployed;
+    public AudioClip ChenSlash;
+    public AudioClip Chen3;
+    private AudioSource Audio;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Audio = GetComponent<AudioSource>();
+        Audio.PlayOneShot(ChenDeployed);
         error = new Vector3(0.6f, -2.3f, 0);
         StartCoroutine(ChenSkill3());
     }
@@ -30,11 +36,14 @@ public class SpawnSlash : MonoBehaviour
         Pos = heart.position -error ;
         float i = Random.Range(0, 360);
         GameObject D = Instantiate(Slash,Pos , Quaternion.Euler(0,0, i));
+        Audio.PlayOneShot(ChenSlash,0.5f);
     }
     IEnumerator ChenSkill3()
     {
         
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(2f);
+        Audio.PlayOneShot(Chen3);
+        yield return new WaitForSeconds(2f);
         heart = GameObject.FindGameObjectWithTag("Heart").GetComponent<Transform>();
         for (int i = 1; i <= 11; i++)
         {
