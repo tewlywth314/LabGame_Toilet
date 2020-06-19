2ca30956562d0cc3dc7e3f168c143a779137a125
tewlywth314 <61342905+tewlywth314@users.noreply.github.com>
2020-06-19 19:17:03 +0700

    TexasPat

---
+++ Assets/Sprite/GenocideDoc/Texas.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texas : MonoBehaviour
+{
+    public GameObject bladeSpawn;
+    private Animator anim;
+    private Animator Ownanim;
+    private AudioSource Audio;
+    public AudioClip Deployed;
+    public AudioClip Skill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ownanim = GetComponent<Animator>();
+        Audio = GetComponent<AudioSource>();
+        Ownanim.SetBool("End", false);
+       
+        anim = bladeSpawn.GetComponent<Animator>();
+        anim.SetBool("Spawn", false);
+        Audio.PlayOneShot(Deployed);
+        StartCoroutine(startSkill());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator startSkill()
+    {
+        yield return new WaitForSeconds(2f);
+        anim.SetBool("Spawn", true);
+        Audio.PlayOneShot(Skill);
+        yield return new WaitForSeconds(10f);
+        anim.SetBool("Spawn", false);
+        Ownanim.SetBool("End", true);
+    }
+}
---
+++ Assets/Sprite/GenocideDoc/TexasBlade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TexasBlade : MonoBehaviour
+{
+    public GameObject blade;
+    public Transform startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyBullet.Destroy = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnBlade()
+    {
+        
+        Instantiate(blade, startpos);
+
+    }
+}
