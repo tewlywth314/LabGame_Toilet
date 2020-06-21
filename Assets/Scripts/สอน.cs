13480f3b1754db036e1b74ddf9e6cd893d92af79
ittinan99 <61344879+ittinan99@users.noreply.github.com>
2020-06-21 14:03:14 +0700

    add-สอน

---
+++ Assets/Scripts/สอน.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class สอน : MonoBehaviour
+{
+    public GameObject imageteaching;
+    void Start()
+    {
+        imageteaching.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            imageteaching.SetActive(false);
+        }
+    }
+}
